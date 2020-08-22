c9f4b2f56d0fd6860b1fe15434c40c9c83a63bf6
Keith Peters <kp@bit-101.com>
2020-08-22 17:19:50 -0400

    proof of concept

---
+++ version.sh
@@ -0,0 +1,24 @@
+#! /bin/bash 
+
+case $1 in
+java)
+  $1 -version
+  ;;
+
+gcc | rustc)
+  $1 --version
+  ;;
+
+node | perl | lua)
+  $1 -v
+  ;;
+
+python)
+  $1 -V
+  ;;
+
+go)
+  $1 version
+  ;;
+
+esac
