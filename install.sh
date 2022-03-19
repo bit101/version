092446d8a642f6eca677b4834b3e7ab068332f5e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 13:53:35 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cp -f version /usr/local/bin
-
-# man page
-gzip -k ./man/version.1
-sudo mkdir -p /usr/local/man/man1/
-sudo cp ./man/version.1.gz /usr/local/share/man/man1/
-rm ./man/version.1.gz
